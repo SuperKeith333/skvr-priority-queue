6e1bca25ee4d60350738655a6dee64f3242bd3c9
SuperKeith333 <115736211+SuperKeith333@users.noreply.github.com>
2025-11-11 16:57:39 -0500

    Actually Complete!

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 export class PriorityQueue {
     constructor() {
-        console.log("Working")
+        this.data = [];
+        this.priorityFunc = this.minHeap;
+    }
+
+    push(item) {
+        this.data.push(item);
+        this.fizzle(this.data.indexOf(item));
+    }
+
+    fizzle(index) {
+        if (index > 0) {
+            while (this.priorityFunc(this.data[index], this.data[index - 1])) {
+                let item = this.data[index]
+                this.data.splice(index, 1);
+                this.data.splice(index - 1, 0, item)
+
+                index -= 1;
+                
+                if (index <= 0)
+                    break;
+            }
+        }
+    }
+    
+    pop() {
+        return this.data.shift();
+    }
+
+    top() {
+        return this.data[0];
+    }
+
+    isEmpty() {
+        return this.data === 0;
+    }
+
+    setHeapFunc(func) {
+        this.priorityFunc = func
+    }
+
+    minHeap(a, b) {
+        return a < b;
     }
 };
